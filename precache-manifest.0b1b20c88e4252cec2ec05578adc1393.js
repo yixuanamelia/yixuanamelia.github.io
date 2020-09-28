f5bdc9f2160b581104a8b82106a40a523051400b
Yixuan WANG <yiwang@student.42.fr>
2020-09-28 16:34:32 +0200

    ALGOLIA TEST BUILD

---
+++ precache-manifest.0b1b20c88e4252cec2ec05578adc1393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f85ce680af5db47ea604a6d8c52ab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c24d1189794a023c11",
+    "url": "/static/css/main.93539a3d.chunk.css"
+  },
+  {
+    "revision": "b0a501589bac5c95871e",
+    "url": "/static/js/2.3f5d735d.chunk.js"
+  },
+  {
+    "revision": "8f3d064698122480c2e1c9d48efe5ada",
+    "url": "/static/js/2.3f5d735d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c24d1189794a023c11",
+    "url": "/static/js/main.bc2fe37b.chunk.js"
+  },
+  {
+    "revision": "f0e61e3239b65016055b",
+    "url": "/static/js/runtime-main.777dd842.js"
+  }
+]);
